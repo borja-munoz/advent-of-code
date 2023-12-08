a37b60535cb435475a5000e39b0d190be7555ae6
Borja Muñoz <borja.munoz@gmail.com>
2023-12-08 12:15:43 +0100

    Day 1 - Part 2

--- 2023/day-01/src/main.rs
+++ 2023/day-01/src/main.rs
@@ -1,23 +1,70 @@
+use regex::{Match, Regex, RegexSet};
 use std::fs;
 
+fn get_digit(my_string: &str) -> char {
+    let str_digits: Vec<&str> = vec!["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+    if my_string.len() == 1 {
+        return my_string.chars().nth(0).unwrap();
+    }
+    else {
+        let index = str_digits.iter().position(|&r| r == my_string).unwrap();
+        return char::from_digit((index + 1).try_into().unwrap(), 10).unwrap();
+    }
+}
+
 fn main() {
+    let patterns = [
+        r"[0-9]",
+        r"one",
+        r"two",
+        r"three",
+        r"four",
+        r"five",
+        r"six",
+        r"seven",
+        r"eight",
+        r"nine",
+    ];    
+    
+    // Compile a set matching any of our patterns.
+    let set = RegexSet::new(patterns).unwrap();
+
+    // Compile each pattern independently.
+    let regexes: Vec<_> = set
+        .patterns()
+        .iter()
+        .map(|pat| Regex::new(pat).unwrap())
+        .collect();
+    
     let mut calibration_value = 0;
     for line in fs::read_to_string("input.txt").unwrap().lines() {
-        let mut first_digit = 'a';
-        let mut last_digit = 'a';
-        for c in line.to_string().chars() {
-            if c.is_digit(10) {
-                if first_digit == 'a' {
-                    first_digit = c;
-                    last_digit = c;
-                } else {
-                    last_digit = c;
-                }
-            }
+
+        // Matching with a single regex does not work with overlapping expressions
+        // so we need first to compare with a RegexSet in order to get 
+        // all the patterns that are present in the line and then get
+        // the individual byte offsets to get the first and last digits
+        let mut first_offset = usize::MAX;
+        let mut last_offset = usize::MIN;
+        let mut first_digit = '0';
+        let mut last_digit = '0';
+        let matches = set.matches(line);
+        for match_index in matches {
+            // println!("Regex matched: {}", regexes[match_index]);
+            let digit_matches: Vec<Match> = regexes[match_index].find_iter(line).map(|n| n).collect();
+            if digit_matches[0].start() < first_offset {
+                first_offset = digit_matches[0].start();
+                first_digit = get_digit(digit_matches[0].as_str());
+            }                
+            if digit_matches[digit_matches.len() - 1].start() >= last_offset {
+                last_offset = digit_matches[digit_matches.len() - 1].start();            
+                last_digit = get_digit(digit_matches[digit_matches.len() - 1].as_str());
+            }                
         }
+
         let mut my_number = first_digit.to_string();
         my_number.push(last_digit);
         calibration_value += my_number.parse::<i32>().unwrap();
+        println!("{}: {}, {}", line, my_number, calibration_value);
     }
     println!("Calibration value: {}", calibration_value);
 }
